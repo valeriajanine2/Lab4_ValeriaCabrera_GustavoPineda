6d95923fcab79f7a64217532a71e3fbe8555d9cd
Thunderbolt2812 <Gustavopineda95@gmail.com>
2020-08-14 14:28:04 -0600

    Menu actualizado

--- Lab4_ValeriaCabrera_GustavoPineda/src/lab4_valeriacabrera_gustavopineda/Lab4_ValeriaCabrera_GustavoPineda.java
+++ Lab4_ValeriaCabrera_GustavoPineda/src/lab4_valeriacabrera_gustavopineda/Lab4_ValeriaCabrera_GustavoPineda.java
@@ -20,6 +20,7 @@ public class Lab4_ValeriaCabrera_GustavoPineda {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int opcion=0;
+        int menuChoices=5;
         while (opcion!=5){
             boolean flag=true;
             while(flag){
@@ -29,10 +30,18 @@ public class Lab4_ValeriaCabrera_GustavoPineda {
                     System.out.println("2. Modificar delito");
                     System.out.println("3. Eliminar delito");
                     System.out.println("4. Listar todo");
-                    opcion=input.nextInt();
+                    System.out.println("5. Salir corriendo");
+                    opcion=Integer.parseInt(input.nextLine());
                     flag=false;
-                }catch(Exception e){
-                    System.out.println("El dato debe de ser un numero entero\nPor favor intente de nuevo");
+                }catch(NumberFormatException e){
+                    System.out.println("El dato debe de ser un numero entero,Por favor intente de nuevo");
+                }while(opcion<0||opcion>menuChoices) {
+                    System.out.println("El dato debe de ser dentro de los numeros del menu");
+                    try{
+                        opcion=Integer.parseInt(input.nextLine());
+                    }catch(NumberFormatException e){
+                        System.out.println("El dato debe de ser un numero entero,Por favor intente de nuevo");
+                    }
                 }
             }
             switch(opcion){
